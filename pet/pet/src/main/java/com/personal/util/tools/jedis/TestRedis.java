a5b574c2b18c7ed7959ac88ea3b24f912947eab5
zfxue <zfxue@99f4572e-9759-4c71-add2-23162d14addb>
2017-04-20 08:57:24 +0000

    add redis and jedis

---
+++ pet/pet/src/main/java/com/personal/util/tools/jedis/TestRedis.java
@@ -0,0 +1,22 @@
+package com.personal.util.tools.jedis;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.personal.util.tools.jedis.CacheFunc;
+import com.personal.util.tools.jedis.RedisBase;
+
+@Service("testRedis")
+public class TestRedis {
+	
+	@Resource(name = "cacheFunc")
+	private CacheFunc cacheFunc = null;
+
+	public void testRedis(){
+		RedisBase redisBase = new RedisBase();
+		redisBase.init();
+		cacheFunc.setStr("zbc123", "薛志�?");
+	}
+
+}
