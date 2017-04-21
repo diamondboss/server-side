e0640bc314dce33cf65788eeead66a56fcba9050
zfxue <zfxue@99f4572e-9759-4c71-add2-23162d14addb>
2017-04-21 09:36:54 +0000

    修改短信签名 呆萌测试==>呆萌博士

--- src/main/java/com/util/sms/aliyun/bean/Constants.java
+++ src/main/java/com/util/sms/aliyun/bean/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	public static final String SMS_ACTION = "";
 	public static final String ALIYUN_ACCESS_KEY = "LTAIiKifYSE1BkLH";
 	public static final String ALIYUN_ACCESS_SECRET = "g1ugTtAGmsICQsVt4tE6kTyI3T9x5r";
-	public static final String ALIYUN_SMS_SIGN = "呆萌测试";
+	public static final String ALIYUN_SMS_SIGN = "呆萌博士";
 	public static final String ALIYUN_SMS_TEMPLATE_CODE = "SMS_62870121";
-	public static final String REDIS_PHONENO_YZM = "";
+	public static final String REDIS_PHONENO_YZM = "redis_phoneno_yzm";
 }
