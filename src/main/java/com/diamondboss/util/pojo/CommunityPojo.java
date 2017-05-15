a9a875f4ab4b0783c36f8f4318ab0f88ac4b4f14
zfxue <zfxue@99f4572e-9759-4c71-add2-23162d14addb>
2017-05-15 04:03:02 +0000

    订单预约

---
+++ src/main/java/com/diamondboss/util/pojo/CommunityPojo.java
@@ -0,0 +1,105 @@
+package com.diamondboss.util.pojo;
+
+import java.util.Date;
+
+public class CommunityPojo {
+    private Long id;
+
+    private String province;
+
+    private String city;
+
+    private String area;
+
+    private String street;
+
+    private String communityName;
+
+    private String imagesUrl;
+
+    private Boolean enable;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province == null ? null : province.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street == null ? null : street.trim();
+    }
+
+    public String getCommunityName() {
+        return communityName;
+    }
+
+    public void setCommunityName(String communityName) {
+        this.communityName = communityName == null ? null : communityName.trim();
+    }
+
+    public String getImagesUrl() {
+        return imagesUrl;
+    }
+
+    public void setImagesUrl(String imagesUrl) {
+        this.imagesUrl = imagesUrl == null ? null : imagesUrl.trim();
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
