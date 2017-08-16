e59fd218d7cbcaed7129dd54a8ce0b0b13363a89
zfxue <zfxue@daimengboshi.com>
2017-08-16 11:24:17 +0800

    [#024]验证发送成功提示

--- src/main/java/com/diamondboss/user/controller/LoginController.java
+++ src/main/java/com/diamondboss/user/controller/LoginController.java
@@ -164,6 +164,7 @@ public class LoginController {
 		if(smsReturnInfo != null && smsReturnInfo.getCode() == StatusCode.SUCCESS_CODE.intValue()){
 			app.setRetnCode(0);
 			log.info("短信验证码发送成功");
+			app.setRetnDesc("发送成功");
 		}else if(smsReturnInfo != null && smsReturnInfo.getCode() == Integer.valueOf(Constants.USE_TOO_MORE)){
 			app.setRetnCode(1);
 			app.setRetnDesc("发送短信频率过高，请稍后再试");
@@ -195,7 +196,7 @@ public class LoginController {
 		} else{
 			app.setData(userOrder);
 			app.setRetnCode(0);
-		}
+		} 
 		return app;
 	}
 	
